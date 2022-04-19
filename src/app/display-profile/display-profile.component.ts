51b1a927960826f6579a65532fe0755505169923
Brenda <bee@bee.com>
2022-04-19 16:24:19 +0300

    Adds the display-profile component

---
+++ src/app/display-profile/display-profile.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+import { User } from '../user';
+import { UserService } from '../user-service/user.service';
+import { Repos } from '../repos';
+
+@Component({
+  selector: 'app-display-profile',
+  templateUrl: './display-profile.component.html',
+  styleUrls: ['./display-profile.component.css'],
+})
+export class DisplayProfileComponent implements OnInit {
+  user!: User;
+  repos!: Repos[];
+
+  getUser(search: any): void {
+    this.userservice.getGithubUser(search.parameters);
+    this.user = this.userservice.user;
+    this.repos = this.userservice.repos;
+  }
+
+  constructor(private userservice: UserService) {}
+
+  ngOnInit(): void {
+    this.userservice.getGithubUser('KellyEgesa');
+    this.user = this.userservice.user;
+    this.repos = this.userservice.repos;
+  }
+}
