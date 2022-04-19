a557b473fb48a6a87d0f588d5c02eac73011ee74
Brenda <bee@bee.com>
2022-04-19 16:25:10 +0300

    Updates the app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,35 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HighlightDirective } from './highlight.directive';
+import { DatePassedPipe } from './date-passed.pipe';
+import { RepositoriesComponent } from './repositories/repositories.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { HomeComponent } from './home/home.component';
+import { FormComponent } from './form/form.component';
+import { DisplayRepositoriesComponent } from './display-repositories/display-repositories.component';
+import { DisplayProfileComponent } from './display-profile/display-profile.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HighlightDirective,
+    DatePassedPipe,
+    RepositoriesComponent,
+    NavbarComponent,
+    HomeComponent,
+    FormComponent,
+    DisplayRepositoriesComponent,
+    DisplayProfileComponent,
+    FormsModule
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+
+  imports: [BrowserModule, AppRoutingModule, HttpClientModule, FormsModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
