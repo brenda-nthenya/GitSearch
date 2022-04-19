92032667750ca40f7b329c81995f4335d2477d21
Brenda <bee@bee.com>
2022-04-19 21:08:44 +0300

    Corrected error forms module error

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
-
+import { RouterModule, Routes } from '@angular/router';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HighlightDirective } from './highlight.directive';
@@ -13,6 +13,7 @@ import { FormComponent } from './form/form.component';
 import { DisplayRepositoriesComponent } from './display-repositories/display-repositories.component';
 import { DisplayProfileComponent } from './display-profile/display-profile.component';
 import { FormsModule } from '@angular/forms';
+// import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -25,7 +26,7 @@ import { FormsModule } from '@angular/forms';
     FormComponent,
     DisplayRepositoriesComponent,
     DisplayProfileComponent,
-    FormsModule
+  
   ],
 
   imports: [BrowserModule, AppRoutingModule, HttpClientModule, FormsModule],
