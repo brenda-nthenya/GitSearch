ecf3ecec8a2e02beedbaa8766bad57b97c4c7601
Brenda <bee@bee.com>
2022-04-19 16:46:30 +0300

    adds routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,18 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { DisplayProfileComponent } from './display-profile/display-profile.component';
+import { DisplayRepositoriesComponent } from './display-repositories/display-repositories.component';
+import { HomeComponent } from './home/home.component';
+
+const routes: Routes = [
+  { path: 'user', component: DisplayProfileComponent },
+  { path: 'repos', component: DisplayRepositoriesComponent },
+  { path: '', component: HomeComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
