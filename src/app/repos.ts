05762a41f78a8224db3c461ba89fc3383fc4749d
Brenda <bee@bee.com>
2022-04-19 16:28:52 +0300

    Updates repos class

---
+++ src/app/repos.ts
@@ -0,0 +1,10 @@
+export class Repos {
+    constructor(
+        public description: string,
+        public id: number,
+        public language: string,
+        public name: string,
+        public url: string,
+        public updatedAt: Date
+      ) {}
+}
